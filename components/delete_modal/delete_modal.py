fb75c7da721ccb3f6adbf331a0e22ce699731683
Robert <robertbaggi@gmail.com>
2025-02-09 20:24:09 +0100

    fix typo in delete-modal and fix icon

--- components/delete_modal/delete_modal.py
+++ components/delete_modal/delete_modal.py
@@ -1,9 +1,9 @@
 from django_components import component
 
 
-@component.register("delete-Model")
+@component.register("delete-modal")
 class DeleteModelComponent(component.Component):
-    template_name = "delete_model/delete_model.html"
+    template_name = "delete_modal/delete_modal.html"
 
     def get_context_data(self, id="delele", text="Sind Sie sicher, dass Sie dieses Element löschen möchten?", post_url="", after_post_url=""):
         return {
